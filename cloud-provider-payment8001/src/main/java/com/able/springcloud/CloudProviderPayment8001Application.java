24236359f59ad59a4421d036c6b5cf0d80ced14b
jipeng <jipeng@able-elec.com>
2020-03-13 18:11:20 +0800

    增加eurekaå¤client配置

--- cloud-provider-payment8001/src/main/java/com/able/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/able/springcloud/CloudProviderPayment8001Application.java
@@ -2,8 +2,9 @@ package com.able.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan(basePackages = {"com.able.springcloud.mapper"})
 public class CloudProviderPayment8001Application {

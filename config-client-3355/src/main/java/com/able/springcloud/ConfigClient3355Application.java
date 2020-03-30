eab75a2d19bce81ee40f265327f2e22eb2faf165
jipeng <jipeng@able-elec.com>
2020-03-30 10:33:49 +0800

    增加config客户端的整合

--- config-client-3355/src/main/java/com/able/springcloud/ConfigClient3355Application.java
+++ config-client-3355/src/main/java/com/able/springcloud/ConfigClient3355Application.java
@@ -3,15 +3,14 @@ package com.able.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class CloudConfigCenter3344Application {
+
+public class ConfigClient3355Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudConfigCenter3344Application.class, args);
+        SpringApplication.run(ConfigClient3355Application.class, args);
     }
 
 }

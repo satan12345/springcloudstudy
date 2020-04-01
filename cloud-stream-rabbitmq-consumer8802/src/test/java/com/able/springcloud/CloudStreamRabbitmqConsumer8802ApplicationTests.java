fb4ad63109cb10ad87c07e8a42907249f14146be
jipeng <jipeng@able-elec.com>
2020-04-01 18:22:24 +0800

    增加cloud-stream消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/test/java/com/able/springcloud/CloudStreamRabbitmqConsumer8802ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.able.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStreamRabbitmqConsumer8802ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

60dc48ae1f257f174b8b3679abcafd7b1ba4d556
jipeng <jipeng@able-elec.com>
2020-04-10 18:44:06 +0800

    搭建整合nacos的模块

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/able/springcloud/CloudalibabaProviderPayment9002Application.java
@@ -0,0 +1,13 @@
+package com.able.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudalibabaProviderPayment9002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderPayment9002Application.class, args);
+    }
+
+}
